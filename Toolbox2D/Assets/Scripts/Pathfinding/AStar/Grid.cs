4e426f53ff9bbd049c5781fa3076886eef441649
Benjamin Rose <rose.benjamin@t-online.de>
2021-05-13 22:38:24 +0200

    started 2D A* implementation

---
+++ Toolbox2D/Assets/Scripts/Pathfinding/AStar/Grid.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    //Sebastian Lague
+    //https://www.youtube.com/watch?v=nhiFx28e7JY&list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW&index=2
+
+
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    [SerializeField] Transform player;
+
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector2 worldBottomLeft = (Vector2)transform.position - Vector2.right * gridWorldSize.x/2 - Vector2.up * gridWorldSize.y/2;
+
+        for(int x = 0; x < gridSizeX; x++)
+        {
+            for(int y = 0; y < gridSizeY; y++)
+            {
+                Vector2 worldPoint = worldBottomLeft + Vector2.right * (x * nodeDiameter + nodeRadius) + Vector2.up * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius, unwalkableMask));
+
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector2 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+        return grid[x, y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
+        if(grid!=null)
+        {
+            Node playerNode = NodeFromWorldPoint(player.position);
+
+            foreach(Node n in grid)
+            {
+                Vector3 worldPos3D = new Vector3(n.worldPosition.x, n.worldPosition.y, 0f);
+
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if(playerNode == n)
+                {
+                    Gizmos.color = Color.cyan;
+                }
+                Gizmos.DrawCube(worldPos3D, Vector3.one * (nodeDiameter-.1f));
+            }
+        }
+    }
+}
